925591cd64e72957dba4f77486c4f33ffb2091a1
Perry Chan <clhperry@gmail.com>
2020-05-08 15:18:47 +0800

    Change database from SQLite3 to PostgreSQL

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ustgraduate',
+        'USER': 'clhperry',
+        'PASSWORD': 'pearypig1104',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
